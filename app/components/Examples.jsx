b7047315d350e9396e3a24d41a377f7c870037a2
Mauro <mbosagn@gmail.com>
2016-12-19 11:22:54 -0300

    foundation classes grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function () {
     return(
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to Examples component</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few examples location to try</p>
+        <ol>
+          <li>
+            <Link to="/?location=Valparaiso">Valparaíso, Chile</Link>
+          </li>
+          <li>
+            <Link to="/?location=Rio">Rio, Brasil</Link>
+          </li>
+
+        </ol>
       </div>
     );
   }
