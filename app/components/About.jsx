0dca8f924814274492ec41f53d698ac64bdadefa
Mauro <mbosagn@gmail.com>
2016-12-19 12:40:44 -0300

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return(
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
+var About = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>App on ReactJS.</p>
+        <p>
+          Here are come some of the tools I used:
+        </p>
+        <ul>
+          <li><a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used</li>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+});
 //
 // var About =  function (props) {
 //   return (
@@ -20,6 +30,6 @@ var React = require('react');
 //   )
 // };
 
-var About = (props) => <h3>About Component!</h3>;
+//var About = (props) => <h3>About Component!</h3>;
 
 module.exports = About;
